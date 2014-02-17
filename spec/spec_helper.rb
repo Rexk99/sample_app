e3c856bb84520ddb6fb6ab0415b1b8319eac6472
Rex Kneisley <rexk_99@yahoo.com>
2014-02-16 23:21:48 -0800

    Added Cucumber features files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -45,8 +45,7 @@ Spork.prefork do
 end
 
 Spork.each_run do
-  # This code will be run each time you run your specs.
-
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 end
 
 # --- Instructions ---
